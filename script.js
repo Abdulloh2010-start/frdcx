94c5799a285ecbf9a25b51297569fd843a3ffffd
Abdulloh2010-start <abdullohlalala@gmail.com>
2025-01-10 18:08:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+let a = confirm=(prompt("enter body backgroundColor"));
+let b = confirm=(prompt("enter text color"));
+document.body.style.backgroundColor = a;
+document.querySelector(".p").style.color = b;
